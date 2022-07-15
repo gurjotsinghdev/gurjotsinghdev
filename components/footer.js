a6831d83ab419d70ba70416fb3865416dc9140cf
gurjotangad97 <gurjotangad97@gmail.com>
2022-07-14 23:06:49 -0700

    footer fix

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer ( ) {
     return (
         <>
             <div className={styles.footer}>
-                <p className={styles.footerTitle}>Built with Next.js</p>
+                <p className={styles.footerTitle}>Built with <u><a href="https://www.youtube.com/watch?v=rmru_nIRf9Q&t=727s">Hukam</a></u>, <u><a href="https://www.youtube.com/watch?v=CZqNR42LjlU&t=43s">Kirtan</a></u> & <u><a href="https://nextjs.org/">Next.js</a></u></p>
                 <p className={styles.footerText}>The source code for this project is <u><a href="https://github.com">available on GitHub</a></u>.</p>
             
             </div>
