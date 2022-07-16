54b7434e31683e9cb2d658cb6c7dab569ead99c9
gurjotangad97 <gurjotangad97@gmail.com>
2022-07-16 15:54:12 -0700

    poke header

--- components/header.js
+++ components/header.js
@@ -8,6 +8,7 @@ import styles from '../styles/Header.module.css'
 export default function Header ( ) {
     return (
         <>
+            
             <div className={styles.header}>
                 <h1 className={styles.logo}> 
                 <Link href="/"><a>G.</a></Link> 
