81ee892d28de5cdb029c3e984cba0a07acac55e4
gurjotangad97 <gurjotangad97@gmail.com>
2022-07-17 16:45:55 -0700

    fav

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
 
   return <>
   <Head>            
-    <title>Gurjot Singh - Software Engineer</title>
+    <title>Pokemon Trainer</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
   </Head>
   <Header />
