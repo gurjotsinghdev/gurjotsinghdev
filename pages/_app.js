ca74ac455860d57ab557c429c1da4176ddf0ace6
gurjotangad97 <gurjotangad97@gmail.com>
2022-07-17 16:51:16 -0700

    title site

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
 
   return <>
   <Head>            
-    <title>Gurjot Singh - Pokémon Trainer</title>
+    <title>Gurjot - Pokémon Trainer</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
   </Head>
   <Header />
