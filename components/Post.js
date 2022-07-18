7fa93fcc3805b76216c62fd98424e36a64708899
gurjotangad97 <gurjotangad97@gmail.com>
2022-07-17 19:04:04 -0700

    image tag fix

--- components/Post.js
+++ components/Post.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Post({ post }) {
   return (
     <div className='card'>
-      <img src={post.frontmatter.cover_image} alt='' />
+      {/* <img src={post.frontmatter.cover_image} alt='' /> */}
 
       <div className='post-date'>Posted on {post.frontmatter.date}</div>
 
