cbaed1fe573c6ba63ae3acb619dbbdcce5ccd74b
gurjotangad97 <gurjotangad97@gmail.com>
2022-07-29 10:44:19 -0700

    social fix

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer ( ) {
     return (
         <>
             <div className={styles.footer}>
-                <p className={styles.footerSocial}><a href="https://www.github.com/gurjotsinghdev">Github</a> - <a href="https://www.behance.net/gurjotsinghdev">Behance</a> - <a href="https://www.youtube.com/watch?v=rmru_nIRf9Q&t=727s">Youtube</a> - <a href="https://www.linkedin.com/in/gurjotsinghdev/">LinkedIn</a> - <a href="https://codepen.io/gurjotsinghdev/">Codepen</a> </p>
+                <p className={styles.footerSocial}><a href="https://www.github.com/gurjotsinghdev">Github</a> - <a href="https://www.behance.net/gurjotsinghdev">Behance</a> - <a href="https://www.youtube.com/channel/UCO1olHpWErqOb9FWzGB8C6w">Youtube</a> - <a href="https://www.linkedin.com/in/gurjotsinghdev/">LinkedIn</a> - <a href="https://codepen.io/gurjotsinghdev/">Codepen</a> </p>
                 <p className={styles.footerText}>Source code&nbsp;<u><a href="https://github.com/gurjotsinghdev/gurjotsinghdev">available on GitHub</a></u>.</p>
              
             </div>
