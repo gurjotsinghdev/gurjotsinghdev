becc8a8d5c7ac3ba6a1cc41e7d90ead12e45e7cd
gurjotangad97 <gurjotangad97@gmail.com>
2022-09-05 16:48:28 -0700

    two new projects added

--- components/allProjects.js
+++ components/allProjects.js
@@ -12,59 +12,82 @@ export default function AllProjects() {
         <>
             <div className={styles.allProjects}>
                 <h1>Selected Projects</h1>
-                <div className={styles.projectsList}>  
+                <div className={styles.projectsList}>
                     <Link href="https://ariadental.com/">
-                    <div className={styles.projectBox}>
-                        <Image
-                            src={project1Img}
-                            alt="Picture of the author"
-                            className={styles.projectImg}
-                            width="400"
-                            height="200"
-                        />
-                        <p className={styles.projectTitle}><b>Aria Dental</b> - UX, Dev </p>
-                    </div>
+                        <div className={styles.projectBox}>
+                            <Image
+                                src={project1Img}
+                                alt="Picture of the author"
+                                className={styles.projectImg}
+                                width="400"
+                                height="200"
+                            />
+                            <p className={styles.projectTitle}><b>Aria Dental</b> - UX, Dev </p>
+                        </div>
+                    </Link><Link href="https://ariadental.com/">
+                        <div className={styles.projectBox}>
+                            <Image
+                                src={project1Img}
+                                alt="Picture of the author"
+                                className={styles.projectImg}
+                                width="400"
+                                height="200"
+                            />
+                            <p className={styles.projectTitle}><b>Aria Dental</b> - UX, Dev </p>
+                        </div>
+                    </Link>
+                    <Link href="https://ariadental.com/">
+                        <div className={styles.projectBox}>
+                            <Image
+                                src={project1Img}
+                                alt="Picture of the author"
+                                className={styles.projectImg}
+                                width="400"
+                                height="200"
+                            />
+                            <p className={styles.projectTitle}><b>Aria Dental</b> - UX, Dev </p>
+                        </div>
                     </Link>
                     <Link href="https://unifypartners.co/">
-                    <div className={styles.projectBox}>
-                        <Image
-                            src={project2Img}
-                            alt="Picture of the author"
-                            className={styles.projectImg}
-                            width="400"
-                            height="200"
-                        />
-                        <p className={styles.projectTitle}><b>Unify Partners</b> - UI, UX, Dev </p>
-                    </div>
+                        <div className={styles.projectBox}>
+                            <Image
+                                src={project2Img}
+                                alt="Picture of the author"
+                                className={styles.projectImg}
+                                width="400"
+                                height="200"
+                            />
+                            <p className={styles.projectTitle}><b>Unify Partners</b> - UI, UX, Dev </p>
+                        </div>
                     </Link>
                     <Link href="https://northcoastskeenafirstnations.ca/">
-                    <div className={styles.projectBox}>
-                        <Image
-                            src={project3Img}
-                            alt="Picture of the author"
-                            className={styles.projectImg}
-                            width="400"
-                            height="200"
-                        />
-                        <p className={styles.projectTitle}><b>North Coast Skeena </b> - UX, Dev </p>
-                    </div>
+                        <div className={styles.projectBox}>
+                            <Image
+                                src={project3Img}
+                                alt="Picture of the author"
+                                className={styles.projectImg}
+                                width="400"
+                                height="200"
+                            />
+                            <p className={styles.projectTitle}><b>North Coast Skeena </b> - UX, Dev </p>
+                        </div>
                     </Link>
                     <Link href="https://fvcl.ca/">
-                    <div className={styles.projectBox}>
-                        <Image
-                            src={project4Img}
-                            alt="Picture of the author"
-                            className={styles.projectImg}
-                            width="400"
-                            height="200"
-                        />
-                        <p className={styles.projectTitle}><b>Fraser Valley Cataract & Laser</b> - Dev </p>
-                    </div>
+                        <div className={styles.projectBox}>
+                            <Image
+                                src={project4Img}
+                                alt="Picture of the author"
+                                className={styles.projectImg}
+                                width="400"
+                                height="200"
+                            />
+                            <p className={styles.projectTitle}><b>Fraser Valley Cataract & Laser</b> - Dev </p>
+                        </div>
                     </Link>
 
                 </div>
                 <Link href="/about">
-                <button className="primaryBtn"><a>About Me</a></button></Link>
+                    <button className="primaryBtn"><a>About Me</a></button></Link>
             </div>
 
         </>
