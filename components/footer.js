39bf8b2e43b467238b7e54a0a5c4bd17bb56c744
Gurjot Singh <49863917+gurjotsinghdev@users.noreply.github.com>
2022-12-25 14:20:36 -0800

    fix social links

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,19 @@
 import Head from 'next/head'
 import styles from '../styles/Footer.module.css'
 
-export default function Footer ( ) {
+export default function Footer() {
     return (
         <>
             <div className={styles.footer}>
-                <p className={styles.footerSocial}><a href="https://www.github.com/gurjotsinghdev">Github</a> - <a href="https://www.behance.net/gurjotsinghdev">Behance</a> - <a href="https://www.youtube.com/channel/UCO1olHpWErqOb9FWzGB8C6w">Youtube</a> - <a href="https://www.linkedin.com/in/gurjotsinghdev/">LinkedIn</a> - <a href="https://codepen.io/gurjotsinghdev/">Codepen</a> - <a href="https://stackoverflow.com/users/17474663/gurjot-singh">Stack Overflow</a> </p>
+                <p className={styles.footerSocial}>
+                    <a href="https://www.youtube.com/@gurjotsinghdev"> Youtube </a> -
+                    <a href="https://www.github.com/gurjotsinghdev"> Github </a> -
+                    <a href="https://codepen.io/gurjotsinghdev/"> Codepen </a> -
+                    <a href="https://stackoverflow.com/users/17474663/gurjot-singh"> Stack Overflow </a> - 
+                    <a href="https://www.linkedin.com/in/gurjotsinghdev/"> LinkedIn </a> </p>
+
                 <p className={styles.footerText}>Source code&nbsp;<u><a href="https://github.com/gurjotsinghdev/gurjotsinghdev">available on GitHub</a></u>.</p>
-             
+
             </div>
         </>
     )
