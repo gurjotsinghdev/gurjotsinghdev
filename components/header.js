d929552e0ef50415e43a2bf3ec82c4da0a54d2fd
Gurjot Singh <49863917+gurjotsinghdev@users.noreply.github.com>
2023-01-08 15:29:31 -0800

    pokeball additions

--- components/header.js
+++ components/header.js
@@ -3,40 +3,45 @@ import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../public/gurjotsinghdev_logo.png'
 import styles from '../styles/Header.module.css'
+import pokeball from '../public/pokeball1.png'
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
-export default function Header ( ) {
+
+export default function Header() {
     return (
-        <>  
-    
+        <>
+
             <div className={styles.header}>
-                <h1 className={styles.logo}> 
-                <Link href="/"><a>
-                <Image
-                        src={logo}
-                        alt="Picture of the author"
-                        className={styles.logo}
-                        width={80}
-                        height={80}
-                    />
-                </a></Link> 
+                <h1 className={styles.logo}>
+                    <Link href="/"><a>
+                        <Image
+                            src={logo}
+                            alt="Picture of the author"
+                            className={styles.logo}
+                            width={80}
+                            height={80}
+                        />
+                    </a></Link>
                 </h1>
                 <div className={styles.mainMenu}>
                     <Link href="/projects"><a>Projects</a></Link>
                     <Link href="/about"><a>About</a></Link>
                     <Link href="/blog"><a>Blog</a></Link>
-                    {/* <Link href="/pokedex"><a>
-                    <Image
+                    <Popup trigger={<button className="pokebutton"> <Image
                         src={pokeball}
                         alt="Picture of the author"
                         className={styles.pokeball}
                         width={40}
                         height={40}
-                    />
-                    </a>
-                    </Link> */}
+                    /> </button>}
+                        position="bottom center">
+                        <div>GeeksforGeeks</div>
+                        <button>Click here</button>
+                    </Popup>
 
                 </div>
-            </div>
+            </div >
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import FeaturedProjects from '../components/featuredProjects'
 import Hero from '../components/hero'
 
+
 export default function Home() {
   return (
     <>
