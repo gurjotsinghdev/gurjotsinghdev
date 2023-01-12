64def0750005c700d2dae4367d0b901a9df104cd
Gurjot Singh <49863917+gurjotsinghdev@users.noreply.github.com>
2023-01-11 19:27:18 -0800

    favicon

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../public/gurjotsinghdev_logo.png'
 import styles from '../styles/Header.module.css'
-import pokeball from '../public/pokeball1.png'
+import pokeball from '../public/pokeball.png'
 import 'reactjs-popup/dist/index.css';
 
 let pokepop = () => {
--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   return <>
   <Head lang="en">            
     <title>Gurjot - Software Engineer</title>
-    <link rel="icon" href="../public/pokeball1.png" />
+    <link rel="icon" href="favicon.png" />
     <meta key="description" name="description" content="initial-scale=1.0, width=device-width" />
   </Head>
   <Header />
